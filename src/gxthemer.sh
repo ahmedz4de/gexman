90ee583e5b49759379e2544789546a7fefc47814
ahmedz4de <rustamahmadzada@gmail.com>
2026-08-15 20:44:37 +0400

    Add an option to uninstall system extensions

--- src/gxthemer.sh
+++ src/gxthemer.sh
@@ -9,6 +9,10 @@ uninstall_extensions(){
 	dconf reset -f /org/gnome/shell/extensions/
 }
 
+uninstall_sysextensions(){
+	sudo rm -rf /usr/share/gnome-shell/extensions
+}
+
 save_theme(){
 	mkdir ../themes/"$1"
 	gnome-extensions list --user > ../themes/"$1"/extensions.txt
@@ -32,6 +36,9 @@ main(){
 	if [ "$1" == "uninstall-extensions" ]; then
 		uninstall_extensions
 		echo "Extensions uninstalled."
+	elif [ "$1" == "uninstall-sysextensions" ]; then
+		uninstall_sysextensions
+		echo "System extensions uninstalled. Logout or reboot to see the changes."
 	elif [ "$1" == "save-theme" ]; then
 		save_theme "$2"
 		echo "Theme '$2' saved."
@@ -46,6 +53,7 @@ main(){
 		echo "Commands:"
 		echo "  help                             Show this information"
 		echo "  uninstall-extensions             Uninstall all user-installed extensions"
+		echo "  uninstall-sysextensions          Uninstall all system-installed extensions. Requires sudo."
 		echo "  save-theme                       Save extension names and settings to a directory with the specified name"
 		echo "  apply-theme                      Apply a theme with the specified name"
 		echo
